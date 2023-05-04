ea28d1355d6517c840b10ed535f5c505fee1e5dd
NGOCLINH-NLINH <124993579+NGOCLINH-NLINH@users.noreply.github.com>
2023-05-04 22:22:49 +0700

    Add files via upload

---
+++ Gridcell.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+#include "Button.h"
+
+class Cell : public Button
+{
+	char CharNumber;
+
+public:
+	bool isEditable;
+	Cell();
+
+	void setNumber(const int number);
+	int getNumber() const;
+
+	void setEditable(const bool editable);
+	void handleKeyboardEvent(const SDL_Event* event, SDL_Texture* textureCache[], int correctNum, int& mistakeFlag);
+
+};
